2cf474c0d0430625264c0ef089579d7fbc8e4f54
StackDoubleFlow <ojaslandge@gmail.com>
2020-02-17 18:02:37 -0600

    Fix debugging

--- entry.js
+++ entry.js
@@ -1,7 +1,9 @@
 try {
     require('@babel/register')({
         presets: ['@babel/preset-env'],
-        plugins: ["@babel/plugin-syntax-bigint"]
+        plugins: ["@babel/plugin-syntax-bigint"],
+        sourceMaps: "inline",
+        retainLines: true
     });
 
     module.exports = require('./server.js');
